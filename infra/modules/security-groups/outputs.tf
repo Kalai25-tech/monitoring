41a6a646626c5584a2bb7c745789914ab57e213d
Kalaiyarasan Jothimani <kjothimani@cog.int>
2026-09-09 23:29:08 -0500

    test

--- infra/modules/security-groups/outputs.tf
+++ infra/modules/security-groups/outputs.tf
@@ -1,14 +1,9 @@
-output "lb_sg_id" {
+output "grafana" {
   description = "Security group ID for the load balancer"
-  value       = aws_security_group.lb.id
+  value       = aws_security_group.grafana.id
 }
 
-output "app_sg_id" {
+output "prometheus" {
   description = "Security group ID for the app tier"
-  value       = aws_security_group.app.id
-}
-
-output "backend_sg_id" {
-  description = "Security group ID for the backend tier"
-  value       = aws_security_group.backend.id
+  value       = aws_security_group.prometheus.id
 }
