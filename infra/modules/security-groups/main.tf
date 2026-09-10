605b9936b01f6198c65573c194430041cdea467d
Kalaiyarasan Jothimani <kjothimani@cog.int>
2026-09-09 23:27:17 -0500

    test

--- infra/modules/security-groups/main.tf
+++ infra/modules/security-groups/main.tf
@@ -17,119 +17,71 @@ resource "aws_security_group" "prometheus" {
 }
 
 
-# lb_http
+# grafana
 
-resource "aws_vpc_security_group_ingress_rule" "lb_http" {
-  security_group_id = aws_security_group.lb.id
+resource "aws_vpc_security_group_ingress_rule" "grafana_console" {
+  security_group_id = aws_security_group.grafana.id
 
-  from_port   = 80
-  to_port     = 80
-  ip_protocol = "tcp"
-
-  cidr_ipv4 = "0.0.0.0/0"
-}
-
-# app-sg
-
-resource "aws_vpc_security_group_ingress_rule" "app_tomcat" {
-  security_group_id = aws_security_group.app.id
-
-  from_port                    = 8080
-  to_port                      = 8080
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.lb.id
-}
-
-# For test - app-sg
-
-resource "aws_vpc_security_group_ingress_rule" "app_tomcat1" {
-  security_group_id = aws_security_group.app.id
-
-  from_port                    = 8080
-  to_port                      = 8080
-  ip_protocol                  = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "app_ssh" {
-  security_group_id = aws_security_group.app.id
-
-  from_port   = 22
-  to_port     = 22
+  from_port   = 3000
+  to_port     = 3000
   ip_protocol = "tcp"
 
   cidr_ipv4 = "0.0.0.0/0"
 }
 
 
-# backend
+resource "aws_vpc_security_group_ingress_rule" "grafana_ssh" {
+  security_group_id = aws_security_group.grafana.id
 
-resource "aws_vpc_security_group_ingress_rule" "backend_mysql" {
-  security_group_id = aws_security_group.backend.id
-
-  from_port                    = 3306
-  to_port                      = 3306
+  from_port                    = 22
+  to_port                      = 22
   ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.app.id
+  cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "backend_memcached" {
-  security_group_id = aws_security_group.backend.id
+# prometheus
 
-  from_port                    = 11211
-  to_port                      = 11211
+resource "aws_vpc_security_group_ingress_rule" "prometheus_ssh" {
+  security_group_id = aws_security_group.prometheus.id
+
+  from_port                    = 22
+  to_port                      = 22
   ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.app.id
+  cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "backend_rabbitmq" {
-  security_group_id = aws_security_group.backend.id
+resource "aws_vpc_security_group_ingress_rule" "prometheus_console_from_public" {
+  security_group_id = aws_security_group.prometheus.id
 
-  from_port                    = 5672
-  to_port                      = 5672
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.app.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "backend_ssh" {
-  security_group_id = aws_security_group.backend.id
-
-  from_port   = 22
-  to_port     = 22
+  from_port   = 9090
+  to_port     = 9090
   ip_protocol = "tcp"
 
   cidr_ipv4 = "0.0.0.0/0"
 }
 
 
-# Allow backend_self
-resource "aws_vpc_security_group_ingress_rule" "backend_self" {
-  security_group_id = aws_security_group.backend.id
+resource "aws_vpc_security_group_ingress_rule" "prometheus_console_from_grafana" {
+  security_group_id = aws_security_group.prometheus.id
 
-  ip_protocol = "-1"
-  referenced_security_group_id = aws_security_group.backend.id
+  from_port                    = 9090
+  to_port                      = 9090
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.grafana.id
 }
 
 
-# LB outbound
-resource "aws_vpc_security_group_egress_rule" "lb_all" {
-  security_group_id = aws_security_group.lb.id
+# grafana outbound
+resource "aws_vpc_security_group_egress_rule" "grafana_outbound" {
+  security_group_id = aws_security_group.grafana.id
 
   ip_protocol = "-1"
   cidr_ipv4   = "0.0.0.0/0"
 }
 
-# App outbound
-resource "aws_vpc_security_group_egress_rule" "app_all" {
-  security_group_id = aws_security_group.app.id
-
-  ip_protocol = "-1"
-  cidr_ipv4   = "0.0.0.0/0"
-}
-
-# Backend outbound
-resource "aws_vpc_security_group_egress_rule" "backend_all" {
-  security_group_id = aws_security_group.backend.id
+# prometheus outbound
+resource "aws_vpc_security_group_egress_rule" "prometheus_outbound" {
+  security_group_id = aws_security_group.prometheus.id
 
   ip_protocol = "-1"
   cidr_ipv4   = "0.0.0.0/0"
